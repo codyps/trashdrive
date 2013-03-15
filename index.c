b24f82edda5869cd5864b6cc79e3e3a6df9b1125
Cody P Schafer <jmesmon@gmail.com>
2013-03-15 02:26:56 -0700

    take a new direction.

---
+++ index.c
@@ -0,0 +1,96 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include <unistd.h>
+
+#include <tommyds/tommy.h>
+
+struct sync_path {
+	char const *dir_path;
+	int dir_fd;
+
+	/* we put all files in 1 hash table, no trees here. */
+	/* FIXME: I'm a lazy bum */
+	tommy_hash_lin entries;
+
+	pthread_t io_th;
+};
+
+
+void *sp_index_daemon(void *varg)
+{
+	struct sync_path *sp = varg;
+	size_t name_max = pathconf(sp->dir_path, _PC_NAME_MAX);
+	if (name_max == -1)
+		name_max = 255;
+	len = offsetof(struct dirent, d_name) + name_max + 1;
+	struct dirent *entry = malloc(len);
+	if (!entry) {
+		fprintf(stderr, "failed to allocate entry\n");
+		exit(1);
+	}
+
+	DIR *dir = fdopendir(sp->dir_fd);
+	if (!dir) {
+		fprintf(stderr, "something went wrong.\n");
+		exit(1);
+	}
+
+	for (;;) {
+		struct dirent entry, *result;
+		int r = readdir_r(dir, &entry, &result);
+		if (r) {
+			fprintf(stderr, "readdir_r() failed: %s\n", strerror(errno));
+			exit(1);
+		}
+
+		if (!entry)
+			break;
+	}
+}
+
+int sp_open(struct sync_path *sp, char const *path)
+{
+	*sp = typeof(*sp) {
+		.dir_path = path,
+	};
+
+
+	int r = open(sp->dir_path, O_RDONLY);
+	if (r == -1)
+		return -1;
+
+	sp->dir_fd = r;
+
+	tommy_hashlin_init(&sp->entries);
+
+	/* should we spawn a thread to handle the scanning of the directory? I think so */
+	r = pthread_create(&sp->io_th, NULL, sp_index_daemon, sp);
+	if (r)
+		return r;
+
+	return 0;
+}
+
+void usage(void)
+{
+	fprintf(stderr, "usage: %s <sync path>\n", prog_name);
+	exit(1);
+}
+
+int main(int argc, char **argv)
+{
+	if (argc != 2) {
+		usage();
+	}
+
+	struct sync_path sp;
+	int r = sp_open(&sp, argv[1]);
+	if (r) {
+		fprintf(stderr, "could not open path \"%s\": %s\n", argv[1], strerror(errno));
+		return 1;
+	}
+
+	return 0;
+}
